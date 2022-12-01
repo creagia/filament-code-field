66ce6632e6d0b74b89ef2b521e9af1f96af14457
Ricard Pons <ricard@creagia.com>
2022-12-01 12:12:46 +0100

    refactor: clean TestCase file

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -31,10 +31,5 @@ class TestCase extends Orchestra
     public function getEnvironmentSetUp($app)
     {
         config()->set('database.default', 'testing');
-
-        /*
-        $migration = include __DIR__.'/../database/migrations/create_filament-json-field_table.php.stub';
-        $migration->up();
-        */
     }
 }

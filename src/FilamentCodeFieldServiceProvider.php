6cdb967ba427864599632f82c1ceadd6c7de1896
Sebastiaan Kloos <sebastiaan@codecycler.com>
2023-05-24 11:13:18 +0200

    initial add support for v3

--- src/FilamentCodeFieldServiceProvider.php
+++ src/FilamentCodeFieldServiceProvider.php
@@ -2,17 +2,37 @@
 
 namespace Creagia\FilamentCodeField;
 
-use Filament\PluginServiceProvider;
+use Filament\Support\Assets\AssetManager;
+use Filament\Support\Assets\Css;
+use Filament\Support\Assets\Js;
+use Filament\Support\Facades\FilamentAsset;
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
 
-class FilamentCodeFieldServiceProvider extends PluginServiceProvider
+
+class FilamentCodeFieldServiceProvider extends PackageServiceProvider
 {
     public static string $name = 'filament-code-field';
 
-    protected array $styles = [
-        'filament-code-field-styles' => __DIR__.'/../resources/dist/filament-code-field.css',
-    ];
+    public function configurePackage(Package $package): void
+    {
+        $package
+            ->name(static::$name)
+            ->hasViews();
+    }
 
-    protected array $beforeCoreScripts = [
-        'filament-code-field-scripts' => __DIR__.'/../resources/dist/filament-code-field.js',
-    ];
+    public function packageRegistered(): void
+    {
+        $this->app->resolving(AssetManager::class, function () {
+            FilamentAsset::register($this->getAssets(), static::$name);
+        });
+    }
+
+    protected function getAssets(): array
+    {
+        return [
+            Css::make('filament-code-field-styles', __DIR__.'/../resources/dist/filament-code-field.css'),
+            Js::make('filament-code-field-scripts', __DIR__.'/../resources/dist/filament-code-field.js'),
+        ];
+    }
 }

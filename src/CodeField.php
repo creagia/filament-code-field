42a6e359156c03fb6713916eea22698c198cc7f0
ricardcreagia <ricardcreagia@users.noreply.github.com>
2024-10-31 09:32:18 +0000

    Fix styling

--- src/CodeField.php
+++ src/CodeField.php
@@ -7,18 +7,18 @@ use Creagia\FilamentCodeField\Concerns\ControlsHeight;
 use Creagia\FilamentCodeField\Concerns\HasDisplayMode;
 use Creagia\FilamentCodeField\Concerns\LineNumbers;
 use Creagia\FilamentCodeField\Concerns\ProgrammingLanguages;
-use Filament\Forms\Components\Field;
 use Filament\Forms\Components\Concerns;
 use Filament\Forms\Components\Contracts;
+use Filament\Forms\Components\Field;
 
 class CodeField extends Field implements Contracts\CanBeLengthConstrained
 {
     use Autocompletion;
+    use Concerns\CanBeLengthConstrained;
     use ControlsHeight;
     use HasDisplayMode;
     use LineNumbers;
     use ProgrammingLanguages;
-    use Concerns\CanBeLengthConstrained;
 
     const PHP = 'php';
 
